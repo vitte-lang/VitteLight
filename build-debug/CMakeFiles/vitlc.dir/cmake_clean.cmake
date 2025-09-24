4abab91302c9234f889f3cf68287dfaab75019dc
Vincent Rousseau <vincentrspro@gmail.com>
2025-09-24 21:49:06 +0200

    BUILD!

---
+++ build-debug/CMakeFiles/vitlc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vitlc.dir/compiler/vitlc.c.o"
+  "CMakeFiles/vitlc.dir/compiler/vitlc.c.o.d"
+  "vitlc"
+  "vitlc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/vitlc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
