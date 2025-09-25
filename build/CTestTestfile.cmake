5b6d16ed48f0f4da8507c54f5f6717f204ca931c
Vincent Rousseau <vincentrspro@gmail.com>
2025-09-25 03:42:19 +0200

    pk-config build .

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/vincent/Documents/Github/VitteLight
+# Build directory: /Users/vincent/Documents/Github/VitteLight/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(run_vl_test "/Users/vincent/Documents/Github/VitteLight/build/vl_test")
+set_tests_properties(run_vl_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/vincent/Documents/Github/VitteLight/CMakeLists.txt;73;add_test;/Users/vincent/Documents/Github/VitteLight/CMakeLists.txt;0;")
